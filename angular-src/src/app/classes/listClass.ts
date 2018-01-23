8e4d23de3ba6707a77c00e7e3067d4cf4193bf2b
abdullahshaaban <abdullah.shaaban91@gmail.com>
2018-01-22 21:01:25 -0800

    Fixed up error in compiling

--- angular-src/src/app/classes/listClass.ts
+++ angular-src/src/app/classes/listClass.ts
@@ -1,11 +1,11 @@
 export class List {
-    public _id: Number;
+    public _id: number;
     public name: String;
     public description: String;
-    public parent_list_id: Number;
-    public children_list_counter: Number;
-    public done: Number;
-    public created_by: Number;
+    public parent_list_id: number;
+    public children_list_counter: number;
+    public done: number;
+    public created_by: number;
 
     constructor(){
        
